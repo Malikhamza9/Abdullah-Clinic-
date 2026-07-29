10566f94fbf6b60a4e1e7289433b49e71f55e416
Malikhamza9 <hmlove6667@gmail.com>
2026-07-29 16:54:38 +0500

    Add appointment alert to script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// ===================================
+// Abdullah Clinic - Script Part 1
+// ===================================
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const appointmentButton = document.querySelector("button");
+
+    appointmentButton.addEventListener("click", function () {
+        alert("Appointment System Coming Soon");
+    });
+
+});
